96bc3cbaff86baf6a026d19518eaed322624f1d5
Josh Michael Olea <34050740+joshmichael23@users.noreply.github.com>
2024-01-09 01:41:48 +0800

    2024-01-09 changes to title and readme

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,6 +4,8 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <title>Chat App Challenge</title>
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       <body>
         <Main />
         <NextScript />
